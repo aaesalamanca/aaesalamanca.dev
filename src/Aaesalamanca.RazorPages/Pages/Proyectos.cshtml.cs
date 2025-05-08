f80f2fdc0a948c95ee55f64afe95451cb3066f81
Alejandro Estornell <aaesalamanca@gmail.com>
2025-05-08 16:40:12 +0200

    feat: Web layout (#4)
    * docs: Fix README command for running the project
    
    * refactor: Remove Bootstrap references, add modern-normalize & add Inter font
    
    * feat: Setup initial web layout with images and icons

---
+++ src/Aaesalamanca.RazorPages/Pages/Proyectos.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class ProyectosModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
