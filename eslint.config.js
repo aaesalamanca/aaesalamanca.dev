5eb5d58165af8578af3d8c2f8b1a40fa7cfafcc4
Alejandro Estornell <aaesalamanca@gmail.com>
2025-05-17 00:41:10 +0200

    refactor: Move to astro (#5)
    * refactor: Remove almost all ASP.NET references & Add Astro
    
    * build: Add ESLint
    
    * build: Add Stylelint
    
    * style: Add & Apply Prettier
    
    * refactor: Complete migration to Astro

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// https://ota-meshi.github.io/eslint-plugin-astro/user-guide/#-usage
+import eslintPluginAstro from "eslint-plugin-astro";
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+];
