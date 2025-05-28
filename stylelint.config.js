e9827f30dd3869e31e248212ae5e69f4d8c0d8ba
Alejandro Estornell <aaesalamanca@gmail.com>
2025-05-28 19:38:09 +0200

    refactor: Back to ASP.NET Razor (#7)
    * refactor: Migrate back to ASP.NET Razor Pages
    
    * fix: Delete SRI for modern-normalize
    
    * feat: Improve profile readability
    
    * docs: Improve README readability
    
    * build: Remove HTTP launch config

--- stylelint.config.js
+++
@@ -1,6 +0,0 @@
-// https://stylelint.io/user-guide/get-started#using-a-custom-syntax-directly
-// https://github.com/ota-meshi/stylelint-config-html?tab=readme-ov-file#book-usage
-/** @type {import('stylelint').Config} */
-export default {
-  extends: ["stylelint-config-standard", "stylelint-config-html/astro"],
-};
