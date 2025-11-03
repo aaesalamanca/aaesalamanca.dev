58cf0f4ce8dad209e55dccb59a5f757e89f776a3
aaesalamanca <aaesalamanca@gmail.com>
2025-11-03 15:19:16 +0100

    refactor: Redesign & Migrate to Bootstrap

--- src/Aaesalamanca.RazorPages/Pages/Services.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace Aaesalamanca.RazorPages.Pages;
-
-public class ServicesModel : PageModel
-{
-    public void OnGet() { }
-}
